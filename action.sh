a1b792a960092ddf86aac62b0b8e76fd972c8c2d
mop <moribuden.mop@gmail.com>
2025-05-29 19:19:07 +0900

    Update action.sh

--- action.sh
+++ action.sh
@@ -225,11 +225,9 @@ function start_vm {
   
   if [[ -n "${proxy_url}" ]]; then
     startup_prelude="${startup_prelude}
-cat <<EOF > /etc/profile.d/proxy.sh
-export http_proxy=${proxy_url}
-export https_proxy=${proxy_url}
-export no_proxy=169.254.169.254,metadata.google.internal,localhost,127.0.0.1,::1
-EOF
+echo 'export http_proxy=${proxy_url}' > /etc/profile.d/proxy.sh
+echo 'export https_proxy=${proxy_url}' >> /etc/profile.d/proxy.sh
+echo 'export no_proxy=169.254.169.254,metadata.google.internal,localhost,127.0.0.1,::1' >> /etc/profile.d/proxy.sh
 chmod +x /etc/profile.d/proxy.sh
 export http_proxy=${proxy_url}
 export https_proxy=${proxy_url}
